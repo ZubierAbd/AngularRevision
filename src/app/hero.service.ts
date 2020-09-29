c4a58450bd7b29a12ea8cc5e56da713cb3c1d1a6
Zubier <zubierabd@gmail.com>
2020-09-28 22:29:33 -0400

    made minor changes

---
+++ src/app/hero.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessagesService } from './messages.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessagesService) { }
+
+  /**Creating an observable out of loading the mock data since we want to simulate a real HTTP service   */
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
-
+import { HeroService } from '../hero.service'
+import { MessagesService } from '../messages.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -9,8 +9,8 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
-  heroes = HEROES;
+  constructor(private heroService: HeroService, private messageService: MessagesService) { }
+  heroes: Hero[];
   selectedHero: Hero;
   hero: Hero = {
     id: 1,
@@ -19,9 +19,17 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => {
+      this.heroes = heroes
+    });
   }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
